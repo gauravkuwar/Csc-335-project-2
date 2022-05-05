b8a87739c9d6ece48cb77fc77139397bf52c6c9d
KEVIN PETER <kpeter1990@yahoo.com>
2022-05-05 14:05:36 -0400

    added main file

---
+++ Csc 335 project 2.scm
@@ -0,0 +1,19 @@
+;; Part 1
+
+;; Design a data type suitable for representing infix propositions, as described in my notes for Class 19.
+
+;; Give a complete specification and development (including a proof) for a program which inputs a proposition
+;; which uses and (^), or (v), not (-) and implies (=>) and which returns a logically equivalent proposition
+;; using just ^ and - .  Both the input and output should use infix notation.
+
+
+;; Part 2
+
+;; Give a complete specification and development (including proof) for an interpreter of infix propositions:
+;; your interpreter will input a proposition and an a-list of T,F values for variables, and will return the
+;; computed value of the input proposition using those values for its variables.
+
+
+;; Part 3
+
+;; Demonstrate your interpreter by using it in conjunction with the front-end of Part 1.
